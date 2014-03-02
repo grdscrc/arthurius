167dd426b892df0bc8deb91ad77bb7305b0af3c3
Igor Descayrac <igor.descayrac@gmail.com>
2014-03-03 00:34:13 +0100

    Refactor with new abstract class, GameLevel

---
+++ levels/GameLevel.java
@@ -0,0 +1,127 @@
+package levels;
+
+import java.awt.Canvas;
+import java.awt.Point;
+import java.util.Iterator;
+import java.util.List;
+
+import characters.Arthurius;
+import characters.Goblin;
+import characters.Wizard;
+import gameframework.base.MoveStrategyDefaultImpl;
+import gameframework.base.MoveStrategyStraightLine;
+import gameframework.game.Game;
+import gameframework.game.GameEntity;
+import gameframework.game.GameLevelDefaultImpl;
+import gameframework.game.GameMovableDriverDefaultImpl;
+import gameframework.game.MoveBlockerChecker;
+import gameframework.game.MoveBlockerCheckerDefaultImpl;
+import gameplay.Boots;
+import gameplay.Door;
+import gameplay.Jail;
+import gameplay.MoveBlockers;
+import gameplay.Mud;
+import gameplay.OverlapRules;
+import gameplay.Projectile;
+import gameplay.Shooter;
+import gameplay.SuperCandy;
+import gameplay.Wall;
+import gameplay.Water;
+import utils.Observer;
+
+public abstract class GameLevel extends GameLevelDefaultImpl implements Observer<Shooter>{
+	Canvas canvas;
+
+	public static final int SPRITE_SIZE = 16;
+	
+	protected Arthurius _Arthurius;
+	protected OverlapRules overlapRules;
+	protected static MoveBlockerChecker moveBlockerChecker = new MoveBlockerCheckerDefaultImpl();
+
+	public GameLevel(Game g) {
+		super(g);
+	}
+	
+	protected void createGoblins(List<Point> goblinsPos, List<Point> goblinsTargets){
+		assert(goblinsPos.size() == goblinsTargets.size());
+		
+		Goblin goblin;
+		for (int t = 0; t < goblinsPos.size(); t++) {
+            GameMovableDriverDefaultImpl goblinDriver = new GameMovableDriverDefaultImpl();
+            MoveStrategyDefaultImpl strat = new MoveStrategyDefaultImpl();
+            goblinDriver.setStrategy(strat);
+            goblinDriver.setmoveBlockerChecker(moveBlockerChecker);
+            goblin = new Goblin(canvas);
+            goblin.setDriver(goblinDriver);
+            goblin.setPosition(goblinsPos.get(t));
+            goblin.setFireDirection(goblinsTargets.get(t));
+            universe.addGameEntity(goblin);
+            overlapRules.addGoblin(goblin);
+            goblin.register(this);
+		}
+	}
+
+	protected void fillUniverse(int[][] tab){
+		// Empty the universe from what was already here
+		Iterator<GameEntity> entities = universe.gameEntities();
+		GameEntity entity;
+		while(entities.hasNext()){
+			entity = entities.next();
+			universe.removeGameEntity(entity);
+		}
+		
+		// Filling up the universe with basic non movable entities and inclusion in the universe
+		for (int i = 0; i < 31; ++i) {
+			for (int j = 0; j < 28; ++j) {
+				if (tab[i][j] == 0) {
+					universe.addGameEntity(new Boots(canvas, new Point(j * SPRITE_SIZE, i * SPRITE_SIZE)));
+				}
+				if (tab[i][j] == 1) {
+					universe.addGameEntity(new Wall(canvas, j * SPRITE_SIZE, i * SPRITE_SIZE));
+				}
+				if (tab[i][j] == 2) {
+					universe.addGameEntity(new SuperCandy(canvas, new Point(j * SPRITE_SIZE, i * SPRITE_SIZE)));
+				}
+				if (tab[i][j] == 4) {
+					universe.addGameEntity(new Jail(new Point(j * SPRITE_SIZE, i * SPRITE_SIZE)));
+				}
+				if (tab[i][j] == 6) {
+					universe.addGameEntity(new Mud(canvas, new Point(j * SPRITE_SIZE, i * SPRITE_SIZE)));
+				}
+				if (tab[i][j] == 7) {
+					universe.addGameEntity(new Water(canvas, j * SPRITE_SIZE, i * SPRITE_SIZE));
+				}
+				if (tab[i][j] == 8) {
+					universe.addGameEntity(new Door(canvas, new Point(j * SPRITE_SIZE, i * SPRITE_SIZE)));
+				}
+			}
+		}
+	}
+	
+	@Override
+	public void update(Shooter shooter) {
+		if(shooter instanceof Goblin){
+			GameMovableDriverDefaultImpl pDriv = new GameMovableDriverDefaultImpl();
+			MoveStrategyStraightLine strat = new MoveStrategyStraightLine(shooter.getPosition(), shooter.getFireDirection());
+			moveBlockerChecker.setMoveBlockerRules(new MoveBlockers());
+			pDriv.setStrategy(strat);
+			pDriv.setmoveBlockerChecker(moveBlockerChecker);
+			Projectile p = new Projectile(canvas, shooter);
+			p.setDriver(pDriv);
+			p.setPosition(shooter.getPosition());
+			universe.addGameEntity(p);
+		}
+		else if(shooter instanceof Wizard){
+			GameMovableDriverDefaultImpl projectileDriver = new GameMovableDriverDefaultImpl();
+			MoveStrategyStraightLine strat = new MoveStrategyStraightLine(shooter.getPosition(), _Arthurius.getPosition());
+			moveBlockerChecker.setMoveBlockerRules(new MoveBlockers());
+			projectileDriver.setStrategy(strat);
+			projectileDriver.setmoveBlockerChecker(moveBlockerChecker);
+			Projectile projectile = new Projectile(canvas, shooter);
+			projectile.setDriver(projectileDriver);
+			projectile.setPosition(shooter.getPosition());
+			universe.addGameEntity(projectile);
+		}
+	}
+
+}
--- levels/GameLevelOne.java
+++ levels/GameLevelOne.java
@@ -1,49 +1,30 @@
 package levels;
 
 import gameframework.base.MoveStrategyKeyboard;
-import gameframework.base.MoveStrategyStraightLine;
 import gameframework.game.CanvasDefaultImpl;
 import gameframework.game.Game;
-import gameframework.game.GameLevelDefaultImpl;
 import gameframework.game.GameMovableDriverDefaultImpl;
 import gameframework.game.GameUniverseDefaultImpl;
 import gameframework.game.GameUniverseViewPortDefaultImpl;
-import gameframework.game.MoveBlockerChecker;
-import gameframework.game.MoveBlockerCheckerDefaultImpl;
 import gameframework.game.OverlapProcessor;
 import gameframework.game.OverlapProcessorDefaultImpl;
-import gameplay.Boots;
-import gameplay.Door;
-import gameplay.Jail;
 import gameplay.MoveBlockers;
-import gameplay.Mud;
 import gameplay.OverlapRules;
-import gameplay.Projectile;
-import gameplay.Shooter;
-import gameplay.SuperCandy;
 import gameplay.TeleportPairOfPoints;
-import gameplay.Wall;
-import gameplay.Water;
-
-import java.awt.Canvas;
 import java.awt.Point;
 import java.util.ArrayList;
 import java.util.List;
 
 import characters.Arthurius;
-import characters.Goblin;
-import utils.Observer;
 
-public class GameLevelOne extends GameLevelDefaultImpl implements Observer<Shooter>  {
-	Canvas canvas;
-
+public class GameLevelOne extends GameLevel {
 	// 0 : Botte; 1 : Walls; 2 : SuperPacgums; 3 : Doors; 4 : Jail; 5 : empty 6: Mud 7:Water 8:Door
 	// Note: teleportation points are not indicated since they are defined by
 	// directed pairs of positions.
 	static int[][] tab = { 
 		    { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
 			{ 1, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 1 },
-			{ 1, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 1 },
+			{ 1, 5, 5, 5, 5, 5, 5, 8, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 1 },
 			{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 1 },
 			{ 1, 7, 7, 7, 7, 7, 7, 7, 7, 1, 5, 5, 1, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 1 },
 			{ 1, 7, 7, 7, 7, 7, 7, 7, 7, 1, 5, 5, 1, 5, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
@@ -73,12 +54,8 @@ public class GameLevelOne extends GameLevelDefaultImpl implements Observer<Shoot
 			{ 1, 0, 5, 5, 5, 1, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 1, 5, 5, 1, 5, 5, 5, 5, 5, 5, 5, 1 },
 			{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 } };
 
-	public static final int SPRITE_SIZE = 16;
 	public static final int NUMBER_OF_GOBLINS = 4;
 	
-	protected Arthurius _Arthurius;
-	protected static MoveBlockerChecker moveBlockerChecker = new MoveBlockerCheckerDefaultImpl();
-
 	@Override
 	protected void init() {
 		OverlapProcessor overlapProcessor = new OverlapProcessorDefaultImpl();
@@ -86,7 +63,7 @@ public class GameLevelOne extends GameLevelDefaultImpl implements Observer<Shoot
 		moveBlockerChecker.setMoveBlockerRules(new MoveBlockers());
 		
 		
-		OverlapRules overlapRules = new OverlapRules(new Point(14 * SPRITE_SIZE, 17 * SPRITE_SIZE),
+		overlapRules = new OverlapRules(new Point(14 * SPRITE_SIZE, 17 * SPRITE_SIZE),
 				new Point(14 * SPRITE_SIZE, 15 * SPRITE_SIZE), life[0], score[0],
 				stuff, levelCompleted, gameOver);
 		overlapProcessor.setOverlapRules(overlapRules);
@@ -97,50 +74,9 @@ public class GameLevelOne extends GameLevelDefaultImpl implements Observer<Shoot
 		gameBoard = new GameUniverseViewPortDefaultImpl(canvas, universe);
 		((CanvasDefaultImpl) canvas).setDrawingGameBoard(gameBoard);
 
-		int totalNbGums = 0;
+		//fillUniverse according to tab;
+		fillUniverse(tab);
 		
-		// Filling up the universe with basic non movable entities and inclusion in the universe
-		for (int i = 0; i < 31; ++i) {
-			for (int j = 0; j < 28; ++j) {
-				if (tab[i][j] == 0) {
-					universe.addGameEntity(new Boots(canvas, new Point(j * SPRITE_SIZE, i * SPRITE_SIZE)));
-					totalNbGums++;
-				}
-				if (tab[i][j] == 1) {
-					universe.addGameEntity(new Wall(canvas, j * SPRITE_SIZE, i * SPRITE_SIZE));
-				}
-				if (tab[i][j] == 2) {
-					universe.addGameEntity(new SuperCandy(canvas, new Point(j * SPRITE_SIZE, i * SPRITE_SIZE)));
-					totalNbGums++;
-				}
-				if (tab[i][j] == 4) {
-					universe.addGameEntity(new Jail(new Point(j * SPRITE_SIZE, i * SPRITE_SIZE)));
-				}
-				if (tab[i][j] == 6) {
-					universe.addGameEntity(new Mud(canvas, new Point(j * SPRITE_SIZE, i * SPRITE_SIZE)));
-					totalNbGums++;
-				}
-				if (tab[i][j] == 7) {
-					universe.addGameEntity(new Water(canvas, j * SPRITE_SIZE, i * SPRITE_SIZE));
-				}
-				if (tab[i][j] == 8) {
-					universe.addGameEntity(new Door(canvas, new Point(j * SPRITE_SIZE, i * SPRITE_SIZE)));
-					totalNbGums++;
-				}
-			}
-		}
-		overlapRules.setTotalNbGums(totalNbGums);
-
-		// Two teleport points definition and inclusion in the universe
-		// (west side to east side)
-		universe.addGameEntity(new TeleportPairOfPoints(new Point(0 * SPRITE_SIZE, 14 * SPRITE_SIZE), new Point(
-				25 * SPRITE_SIZE, 14 * SPRITE_SIZE)));
-		// (east side to west side)
-		universe.addGameEntity(new TeleportPairOfPoints(new Point(27 * SPRITE_SIZE, 14 * SPRITE_SIZE), new Point(
-				2 * SPRITE_SIZE, 14 * SPRITE_SIZE)));
-		
-		
-		// Pacman definition and inclusion in the universe
 		_Arthurius = Arthurius.getInstance(canvas);
 		GameMovableDriverDefaultImpl arthDriver = new GameMovableDriverDefaultImpl();
 		MoveStrategyKeyboard keyStr = new MoveStrategyKeyboard();
@@ -173,14 +109,7 @@ public class GameLevelOne extends GameLevelDefaultImpl implements Observer<Shoot
 			index++;
 		}
 		
-		Goblin myGoblin;
-		for (int t = 0; t < NUMBER_OF_GOBLINS; ++t) {
-			myGoblin = new Goblin(canvas);
-			myGoblin.setPosition(goblinsPos.get(t));
-			myGoblin.setFireDirection(goblinsFire.get(t));
-			myGoblin.register(this);
-			universe.addGameEntity(myGoblin);
-		}
+		createGoblins(goblinsPos, goblinsFire);
 	}
 
 	public GameLevelOne(Game g) {
@@ -188,17 +117,4 @@ public class GameLevelOne extends GameLevelDefaultImpl implements Observer<Shoot
 		canvas = g.getCanvas();
 	}
 
-	//Créer un projectile quand un Shooter tire
-	@Override
-	public void update(Shooter s) {
-		GameMovableDriverDefaultImpl pDriv = new GameMovableDriverDefaultImpl();
-		MoveStrategyStraightLine strat = new MoveStrategyStraightLine(s.getPosition(), (Point) s.getFireDirection().clone());
-		moveBlockerChecker.setMoveBlockerRules(new MoveBlockers());
-		pDriv.setStrategy(strat);
-		pDriv.setmoveBlockerChecker(moveBlockerChecker);
-		Projectile p = new Projectile(canvas, s);
-		p.setDriver(pDriv);
-		p.setPosition(s.getPosition());
-		universe.addGameEntity(p);
-	}
 }
